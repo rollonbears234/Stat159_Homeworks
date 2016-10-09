175e372571734af8fb8cc273839d2b9bb388f902
Bryan Alcorn <irish.dancers.berkeley@gmail.com>
2016-10-09 12:52:55 -0700

    finished tesing functions

--- hw3/code/functions/regression-functions.R
+++ hw3/code/functions/regression-functions.R
@@ -4,8 +4,12 @@ residual_sum_squares <- function(lm_obj) {
 }
 
 #Takes in lm obj and outputs TSS, number
+#sum( (x - mean(x) )^2 )
 total_sum_squares <- function(lm_obj) {
-  return(sum(predict(lm_obj) - lm_obj$fitted.values))
+  y = lm_obj$residuals + lm_obj$fitted.values
+  y_bar = mean(y)
+  obs = lm_obj$residuals + lm_obj$fitted.values
+  return(sum((obs - y_bar)^2))
 }
 
 
@@ -18,18 +22,20 @@ r_squared <- function(lm_obj) {
 
 #Takes in lm object and returns F-statistic, number
 f_statistic <- function(lm_obj) {
-  p = lm_obj$rank#number of predictors
-  n = length(lm_object$residuals)
+  p = lm_obj$rank - 1#number of predictors
+  n = length(lm_obj$residuals)
   RSS = residual_sum_squares(lm_obj)
   TSS = total_sum_squares(lm_obj)
-  return(((TSS-RSS)/p)/(RSS/(n - p - 1)))
+  top = (TSS-RSS)/p
+  bottom = RSS/(n-p - 1)
+  return(top/bottom)
 }
 
 
 #Takes in RSE and outputs RSE,number
 residual_std_error <- function(lm_obj) {
   p = lm_obj$rank#number of predictors
-  n = length(lm_object$residuals)
+  n = length(lm_obj$residuals)
   RSS = residual_sum_squares(lm_obj)
   return(((1/(n-p-1))*RSS)^.5)
 }
---
+++ hw3/code/test-that.R
@@ -0,0 +1,39 @@
+require(testthat)
+
+source("functions/regression-functions.R")
+source("tests/test-regression.R")
+
+load("../data/regression.RData")
+
+
+#
+test_that( "RSS Test", {
+  x <- residual_sum_squares(rel_tv_sales)
+  answer = (summary(rel_tv_sales)$sigma)^2 * 198
+  expect_that(x, equals(answer, tolerance = .1))
+  })
+
+# Should be 5417
+test_that("TSS Test", {
+  x <- total_sum_squares(rel_tv_sales)
+  expect_that(x, equals(5417, tolerance = .1))
+  })
+
+#Should be .6119 for rel_tv_sales, non-adjusted
+test_that("R Squared Test", {
+  x <- r_squared(rel_tv_sales)
+  answer = summary(rel_tv_sales)$r.squared
+  expect_that(x, equals(answer, tolerance = .1))
+  })
+
+#Should be 312.1 for rel_tv_sales
+test_that("F Statistic Test", {
+  x = f_statistic(rel_tv_sales)
+  expect_that(x, equals(312.1, tolerance = .1))
+  })
+
+#Expect 3.259 for rel_tv_sales
+test_that("Residual Std Error Test", {
+  x <- residual_std_error(rel_tv_sales)
+  expect_that(x, equals(3.259, tolerance = .1))
+  })
