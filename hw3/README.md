f1cfd20073e0934dd2095ea64a8472f800725ccf
Bryan Alcorn <irish.dancers.berkeley@gmail.com>
2016-10-09 02:29:53 -0700

    wrote the functions, on to testing

--- hw3/README.md
+++ hw3/README.md
@@ -1,6 +1,6 @@
-Instructions 
+Instructions
 
-HW03 due on Friday Oct-14 
+HW03 due on Friday Oct-14
 
 Instructions on the course's github repo:
 
@@ -28,5 +28,7 @@ PDF Report:
 
 
 **Note to Professor:**
-   * My License for this is in the main homework repo so I don't have to recreate one for every homework assignemnt since I am using one repo for all my homeworks
+   * My License for this is also the main homework repo so I don't have to recreate one for every homework assignemnt since I am using one repo for all my homeworks
 
+
+<a rel="license" href="http://creativecommons.org/licenses/by/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by/4.0/88x31.png" /></a><br />This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/">Creative Commons Attribution 4.0 International License</a>.
--- hw3/code/functions/regression-functions.R
+++ hw3/code/functions/regression-functions.R
@@ -1,27 +1,35 @@
 #takes in lm object as inout and output RSS, number
 residual_sum_squares <- function(lm_obj) {
-
-
+  return(sum(lm_obj$residuals^2))
 }
 
 #Takes in lm obj and outputs TSS, number
 total_sum_squares <- function(lm_obj) {
-
+  return(sum(predict(lm_obj) - lm_obj$fitted.values))
 }
 
 
 #Takes in lm object and returns R^2, number
 r_squared <- function(lm_obj) {
-
+  RSS = residual_sum_squares(lm_obj)
+  TSS = total_sum_squares(lm_obj)
+  return(1 - RSS/TSS)
 }
 
 #Takes in lm object and returns F-statistic, number
 f_statistic <- function(lm_obj) {
-
+  p = lm_obj$rank#number of predictors
+  n = length(lm_object$residuals)
+  RSS = residual_sum_squares(lm_obj)
+  TSS = total_sum_squares(lm_obj)
+  return(((TSS-RSS)/p)/(RSS/(n - p - 1)))
 }
 
 
 #Takes in RSE and outputs RSE,number
 residual_std_error <- function(lm_obj) {
-  
+  p = lm_obj$rank#number of predictors
+  n = length(lm_object$residuals)
+  RSS = residual_sum_squares(lm_obj)
+  return(((1/(n-p-1))*RSS)^.5)
 }
